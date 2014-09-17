98703e02ade41af96dda021537373a7bffb270cc
Michael Lovci <michaeltlovci@gmail.com>
2014-09-17 15:00:47 -0300

    typo

--- public/scala/src/org/broadinstitute/sting/queue/util/TsccUtils.scala
+++ public/scala/src/org/broadinstitute/sting/queue/util/TsccUtils.scala
@@ -115,10 +115,10 @@ object TsccUtils {
    if (genome == "hg19") {
       retval = "/home/gpratt/clipper/clipper/data/regions/hg19_genes.bed"
    }else if(genome == "mm9") {
-      retval = "/home/gpratt/clipper/clipper/data/region/mm9_genes.bed"
+      retval = "/home/gpratt/clipper/clipper/data/regions/mm9_genes.bed"
    }
    else if(genome == "ce10") {
-      retval = "/home/gpratt/clipper/clipper/data/region/ce10_genes.bed"
+      retval = "/home/gpratt/clipper/clipper/data/regions/ce10_genes.bed"
    }
 
    retval
