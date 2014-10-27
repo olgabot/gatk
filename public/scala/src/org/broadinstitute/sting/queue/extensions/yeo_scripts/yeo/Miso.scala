7a5d1a9ae1d37acce655ade482c1b3c1f53e453d
Michael Lovci <michaeltlovci@gmail.com>
2014-10-26 23:35:52 -0200

    declaring variables is dumb

--- public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/Miso.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/Miso.scala
@@ -7,11 +7,12 @@ import org.broadinstitute.sting.queue.function.CommandLineFunction
 class Miso(@Input inBam: File, @Input indexFile: File, @Argument species: String, @Argument pairedEnd: Boolean, @Output output: File) extends CommandLineFunction {
   override def shortDescription = "miso"
   this.nCoresRequest(16)
-  sh_file = swapExt(output, "", ".sh")
+  var sh_file = swapExt(output, "", ".sh")
   def commandLine = "submit_miso_pipeline.py " +
     required("--bam", inBam) +
     required("--sample-id", inBam.getName.split("""\.""")(0)) +
     required("--genome", species) +
+    required("--do-not-submit") +
     // required("--single-end") +
     // conditional( pairedEnd, "--paired-end") +
     // conditional(!pairedEnd, "--single-end") +
