8d619747e37f6ae7973f3892beb962b7cbcb722f
Gabriel Pratt <gpratt@ucsd.edu>
2014-12-04 15:26:13 -0800

    added sailfish

---
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/Sailfish.scala
@@ -0,0 +1,37 @@
+package org.broadinstitute.sting.queue.extensions.yeo
+
+import org.broadinstitute.sting.commandline._
+import java.io.File
+import org.broadinstitute.sting.queue.function.CommandLineFunction
+
+class Sailfish extends CommandLineFunction {
+  
+  
+  @Input(doc="input fastq file", shortName = "inFastq", fullName = "input_fastq_file", required = true) 
+  var inFastq: File = _
+    
+  @Output(doc="output dir file", shortName = "outDir", fullName = "out_dir_file", required = true) 
+  var outDir: File = _
+  
+  @Argument(doc="sailfish index location", shortName = "index", fullName = "index", required = true) 
+  var index: String = _
+    
+  @Argument(doc="sh script to output", shortName = "shScript", fullName = "ash_script", required = false)
+  var shScript: String = _
+  
+  override def shortDescription = "sailfish"
+  this.nCoresRequest = Option(16)
+   
+  def commandLine = "sailfish_quant.py" +
+  required("-1", inFastq) +
+  required("--out-dir", outDir) +
+  required("--index", index) +
+  required("-n", shScript) +
+  required("--out-sh", shScript) +
+  required("-i", index) +
+  "-p 16 --do-not-submit && echo $(tail -n 2 " +
+  required(shScript) +
+  ") | bash"            
+}
+
+
