79e71f275d0923356ee0859b6c6cf89b1c7d417f
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-02-02 12:12:13 -0800

    revert TsccUtils.scala to upstream/master version

--- public/scala/src/org/broadinstitute/sting/queue/util/TsccUtils.scala
+++ public/scala/src/org/broadinstitute/sting/queue/util/TsccUtils.scala
@@ -19,182 +19,172 @@ import collection.JavaConversions._
 
 object TsccUtils {
 
-  def starGenomeLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+    def starGenomeLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/star_sjdb"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/star"
-    } else if (genome == "mm10") {
+   }else if(genome == "mm10") {
       retval = "/projects/ps-yeolab/genomes/mm10/star_sjdb"
-    } else if (genome == "ce10") {
+   }else if(genome == "ce10") {
       retval = "/projects/ps-yeolab/genomes/ce10/star"
-    } else if (genome == "dm3") {
+   }else if(genome == "dm3") {
       retval = "/projects/ps-yeolab/genomes/dm3/star"
-    }
-    retval
+   }
+   retval
   }
 
-  def sailfishGenomeIndexLocation(genome: String): String = {
-    //returns sailfish location for TSCC
-    var retval = "none"
-    if (genome == "hg19") {
-      retval = "/projects/ps-yeolab/genomes/hg19/sailfish/gencode.v19.pc_lncRNA_transcripts.ercc_fluidigm_spikein.fa_sailfish_index_k31"
-    } else if (genome == "mm10") {
-      retval = "/projects/ps-yeolab/genomes/mm10/sailfish_fixed/gencode.vM2.pc_lncRNA_transcripts.ercc_fluidigm_spikein.gfp.fa_sailfish_index"
-    }
-    retval
-  }
+  def chromSizeLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-  def chromSizeLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
-
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/hg19.chrom.sizes"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/mm9.chrom.sizes"
-    } else if (genome == "ce10") {
+   }else if(genome == "ce10") {
       retval = "/projects/ps-yeolab/genomes/ce10/ce10.chrom.sizes"
-    } else if (genome == "dm3") {
+   }else if(genome == "dm3") {
       retval = "/projects/ps-yeolab/genomes/dm3/dm3.chrom.sizes"
-    }
+   }
 
-    retval
+   retval
   }
 
-  def regionsLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+  def regionsLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/hg19data4"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/mm9data4"
-    }
+   }
 
-    retval
+   retval
   }
 
-  def asStructureLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+  def asStructureLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/hg19data4"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/mm9data4"
-    }
+   }
 
-    retval
+   retval
   }
 
-  def genomeLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+  def genomeLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
+
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/chromosomes/all.fa"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/chromosomes/all.fa"
-    } else if (genome == "dm3") {
+   }else if(genome == "dm3") {
       retval = "/projects/ps-yeolab/genomes/dm3/chromosomes/all.fa"
-    }
-    retval
+   }
+   retval
   }
 
-  def phastconsLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+  def phastconsLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/hg19_phastcons.bw"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/mm9_phastcons.bw"
-    }
+   }
 
-    retval
+   retval
   }
 
-  def genicRegionsLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+  def genicRegionsLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/home/gpratt/clipper/clipper/data/regions/hg19_genes.bed"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/home/gpratt/clipper/clipper/data/regions/mm9_genes.bed"
-    } else if (genome == "ce10") {
+   }else if(genome == "ce10") {
       retval = "/home/gpratt/clipper/clipper/data/regions/ce10_genes.bed"
-    }
+   }
 
-    retval
+   retval
   }
 
-  def gffDbLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+  def gffDbLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/gencode.v17.annotation.gtf.db"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/gencode.vM1.annotation.gtf.db"
-    } else if (genome == "ce10") {
+   }else if(genome == "ce10") {
       retval = "/projects/ps-yeolab/genomes/ce10/WS244.genes.gff3.db"
-    }
+   }
 
-    retval
+   retval
   }
 
-  def gffLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+  def gffLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/gencode_v17/gencode.v17.annotation.gtf"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/gencode.vM1.annotation.gtf"
-    } else if (genome == "ce10") {
+   } else if(genome == "ce10") {
       retval = "/projects/ps-yeolab/genomes/ce10/WS244.genes.gff3"
-    }
+}
 
-    retval
+   retval
   }
 
-  def exonLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+  def exonLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/gencode_v17/gencode.v17.annotation.exons.bed"
-    } else if (genome == "mm9") {
+   }else if(genome == "mm9") {
       retval = "/projects/ps-yeolab/genomes/mm9/Mus_musculus.NCBIM37.64.fixed.exons.bed"
-    } else if (genome == "ce10") {
+   }else if(genome == "ce10") {
       retval = "/projects/ps-yeolab/genomes/ce10/ce10.exons.BED"
-    }
+   }
 
-    retval
+   retval
   }
 
-  def gcLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+def gcLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/gencode.v17.gc.txt"
-    }
-    retval
+   }
+   retval
   }
 
-  def snpDbLocation(genome: String): String = {
-    //Returns star genome Location for TSCC, could eventually be factored out into conf file
+def snpDbLocation(genome: String) : String = {
+  //Returns star genome Location for TSCC, could eventually be factored out into conf file
 
-    var retval = "none"
-    if (genome == "hg19") {
+   var retval = "none"
+   if (genome == "hg19") {
       retval = "/projects/ps-yeolab/genomes/hg19/snp137.txt.gz"
-    }
-    retval
+   }
+   retval
   }
 }
