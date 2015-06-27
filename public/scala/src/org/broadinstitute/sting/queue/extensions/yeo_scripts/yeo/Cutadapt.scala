2ac07acba5c273daab0e5f50633465c137d91c5d
Gabriel Pratt <gpratt@ucsd.edu>
2015-06-26 17:45:14 -0700

    udated cutadapt to include 5' trimming and R2 options

--- public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/Cutadapt.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/Cutadapt.scala
@@ -24,6 +24,18 @@ class Cutadapt extends CommandLineFunction {
   
   @Argument(doc="Adapters to trim anywhere (-b) for cutadapt", shortName = "anywhere", fullName = "anywhere", required = false) 
   var anywhere: List[String] = Nil
+
+  @Argument(doc="Adapters to trim anywhere (-g) for cutadapt", shortName = "five_prime", fullName = "five_prime", required = false) 
+  var five_prime: List[String] = Nil
+
+  @Argument(doc="Adapters to trim anywhere (-g) for cutadapt", shortName = "five_prime2", fullName = "five_prime2", required = false) 
+  var five_prime2: List[String] = Nil
+
+  @Argument(doc="Adapters to trim anywhere (-a) for cutadapt", shortName = "three_prime", fullName = "three_prime", required = false) 
+  var three_prime: List[String] = Nil
+
+  @Argument(doc="Adapters to trim anywhere (-A) for cutadapt", shortName = "three_prime2", fullName = "three_prime2", required = false) 
+  var three_prime2: List[String] = Nil
   
   @Argument(doc="Adapters to trim at front (-f) for cutadapt", shortName = "front", fullName = "front", required = false) 
   var front: List[String] = Nil
@@ -32,14 +44,17 @@ class Cutadapt extends CommandLineFunction {
   var overlap: Int = _
   
   @Argument(doc="error rate", shortName = "error_rate", fullName = "error_rate", required = false) 
-  var error_rate: Int = _
+  var error_rate: Double = _
   
   @Argument(doc="length", shortName = "length", fullName = "length", required = false) 
   var length: Int = _
   
   @Argument(doc="quality_cutoff", shortName = "quality_cutoff", fullName = "quality_cutoff", required = false) 
   var quality_cutoff: Int = _
- 
+
+  @Argument(doc="times", shortName = "times", fullName = "times", required = false) 
+  var times: Int = 2
+
   var anywhere2: List[String] = Nil
   if(inPair != null) {
     anywhere2 = anywhere
@@ -48,11 +63,16 @@ class Cutadapt extends CommandLineFunction {
   //Option[Int] 
   this.wallTime = Option((16 * 60 * 60).toLong)
 
-  def commandLine = "cutadapt -f fastq --match-read-wildcards --times 2" + 
+  def commandLine = "cutadapt -f fastq" + 
+  required("--times", times) + 
   optional("-e", error_rate) + 
   optional("-O", overlap) + 
   optional("--quality-cutoff", quality_cutoff) + 
   optional("-m", length) + 
+  repeat("-a", three_prime) +
+  repeat("-g", five_prime) +
+  repeat("-A", three_prime2) +
+  repeat("-G", five_prime2) + 
   repeat("-b", anywhere) + 
   repeat("-B", anywhere2) + 
   repeat("-f", front) + 
