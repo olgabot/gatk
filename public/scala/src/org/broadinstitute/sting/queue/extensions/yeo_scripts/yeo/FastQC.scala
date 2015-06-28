931452ba8ee1932a17ad1029ed6932f748e61c03
Gabriel Pratt <gpratt@ucsd.edu>
2015-06-28 16:13:25 -0700

    added outdir option for fastqc

--- public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FastQC.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FastQC.scala
@@ -2,12 +2,20 @@ package org.broadinstitute.sting.queue.extensions.yeo
 
 import java.io.File
 import org.broadinstitute.sting.queue.function.CommandLineFunction
-import org.broadinstitute.sting.commandline.Input
+import org.broadinstitute.sting.commandline._
 
-class FastQC(@Input inFastq: File) extends CommandLineFunction {
- this.wallTime = Option((2 * 60 * 60).toLong)
+class FastqQC(@Input inFastq: File, @Argument outDir: String= null) extends CommandLineFunction {
 
+  this.wallTime = Option((2 * 60 * 60).toLong)
+  var out = outDir
+  if (out == null) {
+    out = "`cwd`"
+  }
   override def shortDescription = "FastQC"
-  def commandLine = "fastqc %s -o `cwd`".format(inFastq)
-
-}
+  
+  def commandLine = "fastqc" +
+  required(inFastq) + 
+  required("-o", outDir)
+  
+  
+  }
