79b625ffa331e3c710f904951def7f8861cd9d42
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-08-05 14:16:54 -0700

    increase fastqc time

--- public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FastQC.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FastQC.scala
@@ -5,7 +5,7 @@ import org.broadinstitute.sting.queue.function.CommandLineFunction
 import org.broadinstitute.sting.commandline.Input
 
 class FastQC(@Input inFastq: File) extends CommandLineFunction {
- this.wallTime = Option((2 * 60 * 60).toLong)
+ this.wallTime = Option((4 * 60 * 60).toLong)
 
   override def shortDescription = "FastQC"
   def commandLine = "fastqc %s -o `cwd`".format(inFastq)
